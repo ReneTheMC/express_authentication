a0dfd1c8a18cd015882a3bab03b5bee0d2b6b874
Rene Collins <107972484+ReneTheMC@users.noreply.github.com>
2022-09-10 17:08:24 -0500

    Create env var and add session and flash middleware

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 'use strict';
+const bcrypt = require('bcryptjs');
+
 const {
   Model
 } = require('sequelize');
@@ -45,5 +47,24 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'user',
   });
+
+  user.addHook('beforeCreate', (pendingUser) => {
+    let hash = bcrypt.hashSync(pendingUser.password, 12);
+    pendingUser.password = hash;
+  });
+
+  user.prototype.validatePassword = function(typedPassword) {
+    let isCorrectPassword = bcrypt.compareSync(typedPassword, this.password); //boolean
+
+    return isCorrectPassword;
+  }
+
+  user.prototype.toJSON = function() {
+    let userData = this.get();
+    delete userData.password; // this does not mean its deleted from database, only to view user
+
+    return userData;
+  }
+  
   return user;
 };
